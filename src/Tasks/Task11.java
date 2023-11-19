fd89d1ef834b110f02ac2b15b74134d03cddc8cb
Mishachkld <mishachkld@gmail.com>
2023-11-19 13:55:26 +0200

    Add Task11

--- src/Tasks/Task11.java
+++ src/Tasks/Task11.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Task11 {
+public class Task11 {  /// Сложность алгоритма:
     public static void main(String[] args) {  /// 1455 45 64 6548 84 45 86 465 456123 123
         /// 5 11 6 7
         List<Integer> expression = Task1.scanConsole();
@@ -16,6 +16,27 @@ public class Task11 {
         System.out.println(quickSort(expression1));
     }
 
+    public static List<Integer> quickSort(List<Integer> expression) {
+        if (expression.size() < 2)  /// если длина массива меньше двух, то массив отсортирован
+            return expression;
+
+        int rootItem = expression.get(0); // выбираем опорный элемент
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        for (int i = 1; i < expression.size(); i++) {
+            int item = expression.get(i);
+            if (rootItem > item) // если опопрный элемент больше текущего, то добавляем его в л/ч
+                left.add(item);
+            else // если меньше - в л.ч.
+                right.add(item);
+        }
+        expression = quickSort(left); /// делаем все тоже самое с л.ч. и п.ч. наших массивов
+        expression.add(rootItem);
+        expression.addAll(quickSort(right)); // склеваем все в один массив и возвращаем
+        // System.out.println(expression);
+        return expression;
+    }
+
 
 
 
@@ -23,7 +44,7 @@ public class Task11 {
         if (!expression.isEmpty()) {
             int i = start;
             int j = end;
-            int midItem = expression.get((start + end) / 2);
+            int midItem = expression.get((start + end) / 2); /// выбираем элемент, от которого будем отталкиваться
             while (i <= j) {
                 while (expression.get(i) < midItem) {
                     i++;
@@ -43,25 +64,4 @@ public class Task11 {
                 quickSort(expression, start, j);
         }
     }
-
-    public static List<Integer> quickSort(List<Integer> expression) {
-        if (expression.size() < 2)
-            return expression;
-
-        int rootItem = expression.get(0);
-        List<Integer> left = new ArrayList<>();
-        List<Integer> right = new ArrayList<>();
-        for (Integer item : expression) {
-            if (rootItem > item)
-                left.add(item);
-            else
-                right.add(item);
-        }
-        //left.add(rootItem);
-        expression = quickSort(left);
-        expression.addAll(right);
-        // System.out.println(expression);
-        return expression;
-    }
-
 }
