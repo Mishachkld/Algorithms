92a4a547883f92add6e8c12e76fc4023dbba9388
Mishach <mishachkld@gmail.com>
2024-04-23 13:33:04 +0200

    add Lab9

---
+++ src/SecondSemester/Lab9/Lab9.java
@@ -0,0 +1,16 @@
+package SecondSemester.Lab9;
+
+import Tools.HelpClasses.Graph;
+import Tools.Helper;
+
+import java.io.IOException;
+
+public class Lab9 {
+    public static void main(String[] args) throws IOException {
+        String path = "src/SecondSemester/Lab9/input.txt";
+        Graph graph = new Graph(Helper.readMatrixFromFile(path));
+        int startPoint = 0;
+        graph.bellmanFord(startPoint);
+        System.out.println(graph);
+    }
+}
--- src/Tools/HelpClasses/Graph.java
+++ src/Tools/HelpClasses/Graph.java
@@ -10,6 +10,7 @@ public class Graph {
     private List<Boolean> sptSet;
     private List<Integer> dist;
     private final int vertices;
+    private int startIndex;
 
     public Graph(List<List<Integer>> vertices) {
         this.vertices = vertices.size();
@@ -17,6 +18,7 @@ public class Graph {
     }
 
     public void dekstra(int startIndex) {
+        this.startIndex = startIndex;
         dist = new ArrayList<>(Collections.nCopies(vertices, Integer.MAX_VALUE)); // массив из максимальных значений
         dist.set(startIndex, 0);
         sptSet = new ArrayList<>(Collections.nCopies(vertices, false));
@@ -24,8 +26,8 @@ public class Graph {
             int u = this.findMinDistanceIndex();  // получаем миниальную длину, за которую можем попасть в точку
             sptSet.set(u, true);
             for (int v = 0; v < vertices; v++) {
-                if ((graph.get(u).get(v) > 0) && (!sptSet.get(v)) && (dist.get(v) > dist.get(u) + graph.get(u).get(v))) { // (либо оставляем предыдущее значение, либо вот эта формула)
-                    dist.set(v, dist.get(u) + graph.get(u).get(v));         // перещитываем длинны кратчайших особых путей
+                if ((graph.get(u).get(v) > 0) && (!sptSet.get(v)) && (dist.get(v) > dist.get(u) + graph.get(u).get(v))) { // (либо оставляем предыдущее значение если оно мнимально, либо вот эта формула)
+                    dist.set(v, dist.get(u) + graph.get(u).get(v));         // перещитываем длинны кратчайших особых путей если нашли более короткий путь из текущей вершины
                 }
             }
         }
@@ -33,11 +35,12 @@ public class Graph {
         System.out.println(dist);
     }
 
-    public int findMinDistanceIndex() {
+    private int findMinDistanceIndex() {
         int minDistance = Integer.MAX_VALUE;
         int minIndex = -1;
         for (int i = 0; i < vertices; i++) {
-            if ((dist.get(i) < minDistance) && !(sptSet.get(i))) {
+            if ((dist.get(i) < minDistance) && !(sptSet.get(i))) { // + если элемент в вассиве sptSet имеет false
+                // то текущая дистанция будет минимальной и мы сохраняем индекс элемента с этим значением
                 minDistance = dist.get(i);
                 minIndex = i;
             }
@@ -45,15 +48,35 @@ public class Graph {
         return minIndex;
     }
 
+    public void bellmanFord(int src) {
+        dist = new ArrayList<>(Collections.nCopies(vertices, Integer.MAX_VALUE));
+        dist.set(src, 0);
+
+        for (int loop = 0; loop < vertices - 1; loop++) {
+            for (int u = 0; u < vertices; u++) {
+                for (int v = 0; v < vertices; v++) {
+                    if (graph.get(u).get(v) != 0) {
+                        if (dist.get(u) + graph.get(u).get(v) < dist.get(v)) {
+                            dist.set(v, dist.get(u) + graph.get(u).get(v));
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(dist);
+    }
+
+
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
         if (dist == null) {
-            stringBuilder.append("Call method dijkstra after sout solution!!!");
+            stringBuilder.append("Call method dijkstra or belmanFord after sout solution!!!");
         } else {
             stringBuilder.append("Длина кротчайшего пути от начальной точки до других элементов графа").append("\n");
             for (int i = 0; i < vertices; i++) {
-                stringBuilder.append(i).append(" <-> ")
+                stringBuilder.append(startIndex).append(":")
+                        .append(i).append(" <-> ")
                         .append(dist.get(i)).append("\n");
             }
         }
