9366ae49119dc571ceaf4453d2c0a0d3bd466b52
Mishach <mishachkld@gmail.com>
2024-05-25 16:09:54 +0200

    add Lab18

--- src/SecondSemester/Lab18/Lab18.java
+++ src/SecondSemester/Lab18/Lab18.java
@@ -1,31 +1,54 @@
 package SecondSemester.Lab18;
 
-public class Lab18 {
-    public static boolean subsetSum(int[] nums, int summ) {
-        int n = nums.length;
-        boolean[][] dp = new boolean[n + 1][summ + 1];
+import java.util.Arrays;
 
-        for (int i = 0; i <= n; i++) {
+public class Lab18 {
+
+    // Дано множество целых чисел S и целевое число t.
+    // Требуется определить, существует ли подмножество S' множества S, сумма элементов которого равна целевому числу t.
+
+    // Задача о суммах подмножеств является классической комбинаторной задачей.
+    // Для данного множества целых чисел S мы хотим найти подмножество S', сумма элементов которого равна заданному числу t.
+    // Эта задача решена перебором всех возможных подмножеств множества S.
+
+    // O(n*S), где n - длина массива nums, а S - целевая сумма.
+    public static boolean dinamicSubsetSum(int[] nums, int sum) {
+        int sizeNums = nums.length;
+        boolean[][] dp = new boolean[sizeNums + 1][sum + 1];
+        for (int i = 0; i <= sizeNums; i++) {
             dp[i][0] = true;
         }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= summ; j++) {
-                if (nums[i - 1] > j) {
+        for (int i = 1; i <= sizeNums; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (nums[i - 1] > j) {                      // если текущий элемент больше текущей суммы, то мы берем значение предыдущего элемента
                     dp[i][j] = dp[i - 1][j];
                 } else {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];  //можно ли получить сумму, если тек. элемент будет ключен
+                                                                            // или можно ли получить сумму без текущего элемента
                 }
             }
         }
+        return dp[sizeNums][sum]; /// итоговый реультат
+    }
 
-        return dp[n][summ];
+    // O(n log n), где n - количество элементов в массиве.
+    public static boolean fatCalculateSum(int[] nums, int S) {
+        Arrays.sort(nums); // Сортируем массив
+
+        int sum = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (sum + nums[i] <= S) {
+                sum += nums[i];
+            }
+        }
+
+        return sum == S;
     }
 
     public static void main(String[] args) {
-        int[] nums = {3, 34, 4, 12, 5, 2};
-        int summ = 9;
-        System.out.println(subsetSum(nums, summ));
+        int[] nums = {3, 34, 3, 12, 5, 2};
+        int sum = 3;
+        System.out.println((dinamicSubsetSum(nums, sum)) ? "Существует" : "Не существует");
     }
 
 }
