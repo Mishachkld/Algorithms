bee4f9f7def2fc7f51c64cf8baf12e57718aed34
Mishach <mishachkld@gmail.com>
2024-05-25 14:07:11 +0200

    add Lab17

--- src/SecondSemester/Lab17/Lab17.java
+++ src/SecondSemester/Lab17/Lab17.java
@@ -1,33 +1,40 @@
 package SecondSemester.Lab17;
 
+import java.util.Arrays;
+
 public class Lab17 {
 
-    public static int firstFit(int[] weight, int n, int c) {
-        int res = 0;
-        int[] bin_rem = new int[n];
+    //Дано множество предметов с заданными весами w_1, w_2, ..., w_n и n пустых ящиков.
+    // Требуется распределить все предметы по ящикам таким образом, чтобы минимизировать количество использованных ящиков.
+    // Каждый ящик имеет ограниченную вместимость C, и суммарный вес предметов в каждом ящике не должен превышать эту вместимость
 
-        for (int i = 0; i < n; i++) {
-            int j = 0;
-            while (j < res) {
-                if (bin_rem[j] >= weight[i]) {
-                    bin_rem[j] = bin_rem[j] - weight[i];
+    // функция для подсчета колличества использованных контейнеров
+    public static int firstFit(int[] weights, int n, int containerSize) {  /// с - вместимость контейнера
+        int quantityOfContainers = 0; // колличество использованных контейнеров
+        int[] bin_rem = new int[n]; // массив хранит информацию о том, сколько свободного места осталось в каждом контейнере после размещения предметов.
+        for (int i = 0; i < n; i++) { // проходимся по всем предметам
+            int j;
+            for (j = 0; j < quantityOfContainers; j++) {
+                if (bin_rem[j] >= weights[i]) {
+                    bin_rem[j] = bin_rem[j] - weights[i]; // пытаемся положить в текущий контейнер предмет (обновляем колличество места свободного в текущем контейнере)
                     break;
                 }
-                j++;
             }
-
-            if (j == res) {
-                bin_rem[res] = c - weight[i];
-                res++;
+            if (j == quantityOfContainers) {
+                bin_rem[quantityOfContainers] = containerSize - weights[i]; // если у нас появился не задействованный контейнер,
+                quantityOfContainers++;                                     // то мы кладем предмет в текущую ячейку и делаем пересчет свободного места
             }
         }
-        return res;
+        System.out.println(Arrays.toString(Arrays.stream(bin_rem).toArray()));
+        return quantityOfContainers;
     }
-
+    // худшая сложность - O(n^2), где n - количество предметов
+    // Лучшая - O(n), где n - количество предметов.
     public static void main(String[] args) {
-        int[] weight = {2, 3, 5, 4, 7};
-        int c = 10;
-        int n = weight.length;
-        System.out.println(firstFit(weight, n, c));
+        int[] weights = {3, 5, 7, 4, 2};
+        int containerSize = 10; // вместимость контейнера
+        int countOfElements = weights.length;
+        System.out.println("Необходимо " + firstFit(weights, countOfElements, containerSize) + " контейнера(ов)");
+
     }
 }
